7854dcd89d90df0d103f6012f79c9c09c747be7c
Audu Moses <89123661+MoscoDev@users.noreply.github.com>
2023-01-13 13:04:50 +0100

    security update

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ app.use(morgan('combined', { stream: accessLogStream }))
 /* 
 @description    For CORS (To allow for every request set `corsOption.origin` to true)
 */
-const allowlist = ["http://localhost:5000","http://localhost:5173","https://nextjob.onrender.com"];
+const allowlist = process.env.ALLOW_LIST.split(" ");
 const corsOptionsDelegate = function (req, callback) {
   let corsOptions;
   if (allowlist.indexOf(req.header("Origin")) !== -1) {
